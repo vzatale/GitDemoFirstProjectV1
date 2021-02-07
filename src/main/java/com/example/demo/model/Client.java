b1964f2020d3bcdfab0b69c9fc18e8fda86228d8
User <swapnilp.gceme@gmail.com>
2021-02-07 20:18:02 +0530

    removed setter getter and toString, Added Lombok dependancy

--- src/main/java/com/example/demo/model/Client.java
+++ src/main/java/com/example/demo/model/Client.java
@@ -1,38 +1,12 @@
 package com.example.demo.model;
 
+import lombok.Data;
+
+@Data
 public class Client {
 
 	private int cid;
 	private String cname;
 	private String caddr;
-
-	public int getCid() {
-		return cid;
-	}
-
-	public void setCid(int cid) {
-		this.cid = cid;
-	}
-
-	public String getCname() {
-		return cname;
-	}
-
-	public void setCname(String cname) {
-		this.cname = cname;
-	}
-
-	public String getCaddr() {
-		return caddr;
-	}
-
-	public void setCaddr(String caddr) {
-		this.caddr = caddr;
-	}
-
-	@Override
-	public String toString() {
-		return "Client [cid=" + cid + ", cname=" + cname + ", caddr=" + caddr + "]";
-	}
 
 }
